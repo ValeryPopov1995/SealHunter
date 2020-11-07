bf9081f1a2975bd38df0db7408e827da97c5336d
ValeryPopov1995 <72905449+Valer-jan@users.noreply.github.com>
2020-11-07 11:27:59 +0300

    git created, player movnment added, 3 packes added

---
+++ Assets/Scripts/Player/PlayerMovenment.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovenment : MonoBehaviour
+{
+    public bool isActive = true;
+    public FixedJoystick FixedStick;
+    [Space]
+    public float MoveSpeed = 10f;
+
+    CharacterController character;
+
+    private void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (isActive) moving();
+    }
+
+    void moving()
+    {
+        Vector3 mov = Vector3.forward * Input.GetAxis("Vertical") * MoveSpeed +
+            Vector3.right * Input.GetAxis("Horizontal") * MoveSpeed - Vector3.up * 2;
+        character.Move(mov * Time.deltaTime);
+    }
+}
