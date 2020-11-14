8eb1f4f3bff568949a0f5b50898d7caa82fbdee0
ValeryPopov1995 <72905449+Valer-jan@users.noreply.github.com>
2020-11-14 19:14:46 +0300

    bullet prefab in weapon.cs, bullet.cs added, enemy trigger enter, reloading weapon system

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public float Damage = 10, Rate = 100, ReloadTime = 1f;
+    public float Rate = 100, ReloadTime = 1f;
     public int BulletsMax = 10, BulletsCurrent = 10;
-    public ParticleSystem ShootParticles;
+    public GameObject BulletPrefab;
 
     bool readyToShoot = true, reloading = false;
     [HideInInspector]
@@ -22,27 +22,28 @@ public class Weapon : MonoBehaviour
         {
             readyToShoot = false;
 
-            // magic
-            if (ShootParticles != null) ShootParticles.Play();
-            // shoot
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
-            {
-                // impact
-                // Instantiate(ImpactEffect, hit.transform.position, hit.transform.rotation);
+            Instantiate(BulletPrefab, transform.position, transform.rotation);
 
-                // damage
-                EnemyParams enemy = hit.transform.GetComponent<EnemyParams>();
-                if (enemy != null) enemy.TakeDamage(Damage);
-            }
+            // ray cast shoot
+            //RaycastHit hit;
+            //if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+            //{
+            //    // impact
+            //    // Instantiate(ImpactEffect, hit.transform.position, hit.transform.rotation);
+
+            //    // damage
+            //    EnemyParams enemy = hit.transform.GetComponent<EnemyParams>();
+            //    if (enemy != null) enemy.TakeDamage(Damage);
+            //}
+
             BulletsCurrent--;
             AmmoString = BulletsCurrent.ToString();
-            if (BulletsCurrent < 1) StartCoroutine(reload());
 
-            yield return new WaitForSeconds(60/Rate);
+            yield return new WaitForSeconds(60 / Rate);
             readyToShoot = true;
         }
         
+        if (BulletsCurrent < 1) StartCoroutine(reload());
     }
     IEnumerator reload()
     {
