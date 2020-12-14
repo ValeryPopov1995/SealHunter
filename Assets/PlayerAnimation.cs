7a4b9d15ea04d31ad19b855107b2c621ee752265
ValeryPopov1995 <72905449+Valer-jan@users.noreply.github.com>
2020-12-14 23:48:46 +0300

    player animation

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    Animator anim;
+    FixedJoystick stick;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        stick = GetComponentInParent<PlayerMovenment>().FixedStick;
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("BlendX", stick.Horizontal);
+        anim.SetFloat("BlendY", stick.Vertical);
+    }
+}
